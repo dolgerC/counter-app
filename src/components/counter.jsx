61ef13a7e4e7fec750bea04e3c21384c87c01237
Dolger Campos <dolger.camposch@gmail.com>
2018-08-29 16:29:42 -0600

    Reseting the counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
+  // state = {
+  //   value: this.props.counter.value
+  // };
 
   //   constructor() {
   //     //when an object counter is created
@@ -11,17 +11,20 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this); //set the value of this, will return a new instance of the handledincrement and is going to referencve the counter
   //   }
 
-  handleIncrement = () => {
-    //console.log("EventHandler Called", product);
-    this.setState({ value: this.state.value + 1 }); //simple add to the state count (stateChanges)
-  };
+  // handleIncrement = () => {
+  //   //console.log("EventHandler Called", product);
+  //   this.setState({ value: this.state.value + 1 }); //simple add to the state count (stateChanges)
+  // };
+
+  //deleted the state and the handle increment so we can use the props from the counters array and not the state local in counter
+  //this is done for reseting the values if not the page just simply stay as it is
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)} //{this.handleIncrement} removed since local we need to make it as the ondelete
           className="btn btn-secondary btn-nm"
         >
           Increment
@@ -38,12 +41,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter; // we removed the local state and rely on the props
     return value === 0 ? "Zero" : value;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,24 @@ class Counters extends Component {
     ]
   };
 
+  handleIncrement = counter => {
+    //console.log(counter);
+    const counters = [...this.state.counters]; //clone the array
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    console.log(this.state.counters[0]);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     //console.log("EventHandler Called", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -20,11 +38,18 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            counter={counter}
+            onIncrement={this.handleIncrement}
+            counter={counter} //previewsly it was counter.id to improve it we use the object instead
           />
         ))}
       </div>
