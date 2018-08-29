2a3a5c9d696937b0fbd347813fb1e94df8b02702
Dolger Campos <dolger.camposch@gmail.com>
2018-08-29 16:54:27 -0600

    Lisfting the state up
    Since there is no parent-child relationship between the counters component and the navbar we need tho lift the state up so both of them can use the total nomber of counters[], so we moved to the App so we can pass it to the children via props

--- src/App.js
+++ src/App.js
@@ -4,12 +4,52 @@ import "./App.css";
 import Counters from "./components/counters";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    //console.log(counter);
+    const counters = [...this.state.counters]; //clone the array
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    //console.log(this.state.counters[0]);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    //console.log("EventHandler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
         </main>
       </React.Fragment>
     );
