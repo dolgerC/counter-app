40114250b105ff875c48a9f29811cbea0d57ba90
Dolger Campos <dolger.camposch@gmail.com>
2018-08-29 16:06:06 -0600

    Deleting and updating

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   //   constructor() {
@@ -11,8 +11,9 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this); //set the value of this, will return a new instance of the handledincrement and is going to referencve the counter
   //   }
 
-  handleIncrement = product => {
-    this.setState({ value: this.state.count + 1 }); //simple add to the state count (stateChanges)
+  handleIncrement = () => {
+    //console.log("EventHandler Called", product);
+    this.setState({ value: this.state.value + 1 }); //simple add to the state count (stateChanges)
   };
 
   render() {
@@ -25,19 +26,25 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)} //delete button
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
