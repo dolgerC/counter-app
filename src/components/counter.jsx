61525c3cf580e6e486e8cee12cd7b1915331ed72
Dolger Campos <dolger.camposch@gmail.com>
2018-08-28 18:03:24 -0600

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,8 +9,6 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-nm">Increment</button>
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
@@ -18,17 +16,6 @@ class Counter extends Component {
         </ul>
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
